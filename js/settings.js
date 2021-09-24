fc98bfdcb428f4be5f6b4cc42caca10355401b01
Edwin Okugbo <edwinokugbo@gmail.com>
2021-09-24 22:55:59 +0100

    my first commit

---
+++ js/settings.js
@@ -0,0 +1,8 @@
+const headerBGDef = './img/header_bg1.jpg'
+const headerBG = [
+    './img/header_bg4.jpg',
+    './img/header_bg2.jpg',
+    './img/header_bg3.jpg',
+]
+const slideShowOn = true
+const slideDuration = 7000
